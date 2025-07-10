fac2b239eaa78be3da1af7ac1aafad96159f1778
Калинкин Павел <pnzazfof@gmail.com>
2025-07-10 23:45:08 +0200

    dlt workshop

---
+++ workshops/dlt_workshop/dlt_workshop.py
@@ -0,0 +1,27 @@
+import dlt
+import requests
+
+@dlt.resource
+def zoomcamp_data():
+    docs_url = 'https://github.com/alexeygrigorev/llm-rag-workshop/raw/main/notebooks/documents.json'
+    docs_response = requests.get(docs_url)
+    documents_raw = docs_response.json()
+
+    for course in documents_raw:
+        course_name = course['course']
+
+        for doc in course['documents']:
+            doc['course'] = course_name
+            yield doc
+
+
+qdrant_destination = dlt.destinations.qdrant(qd_path="db.qdrant")
+
+pipeline = dlt.pipeline(
+    pipeline_name="zoomcamp_pipeline",
+    destination=qdrant_destination,
+    dataset_name="zoomcamp_tagged_data"
+
+)
+load_info = pipeline.run(zoomcamp_data())
+print(pipeline.last_trace)
